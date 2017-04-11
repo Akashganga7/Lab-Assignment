cd7223753e16906841dfd924906081470efee5b7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2017-04-11 07:32:50 +0600

    First assignment finished

--- Assignment-1/assignment.rb
+++ Assignment-1/assignment.rb
@@ -1,4 +1,8 @@
 require 'pp'
+require 'mongo'
+
+
+Mongo::Logger.logger.level = ::Logger::INFO;
 
 class Solution
   @@db = nil
@@ -8,20 +12,20 @@ class Solution
     #create a `Mongo::Client` connection to the server using a URL (.e.g., 'mongodb://localhost:27017')
     #configure the client to use the `test` database
     #assign the client to @@db class variable and return that client
-    
+    @@db = Mongo::Client.new([ '127.0.0.1:27017' ]).use(:test);
   end
 
   #Implement a class method in the `Solution` class called `collection` that will
   def self.collection
     #return the `zips` collection
-    
+    @@db[:zips];
   end
 
   #Implement an instance method in the `Solution` class called `sample` that will
   def sample
     #return a single document from the `zips` collection from the database. 
     #This does not have to be random. It can be first, last, or any other document in the collection.
-
+    self.class.collection.find.first();
   end
 end
 
